4c6944dcd01fe98bd06dee67991f26ea8d451245
chengcheng <chengcheng@qcraft.ai>
2022-05-20 20:28:49 +0800

    rm console

--- boa_engine/src/vm/mod.rs
+++ boa_engine/src/vm/mod.rs
@@ -1685,7 +1685,7 @@ impl Context {
             }
         }
         self.vm.cu_cost += as_cost(opcode);
-        js_console_log(&format!("opcode: {}, cost: {}", opcode.as_str(), &self.vm.cu_cost.to_string()));
+        // js_console_log(&format!("opcode: {}, cost: {}", opcode.as_str(), &self.vm.cu_cost.to_string()));
         if self.vm.cu_cost > self.vm.cu_limit.unwrap_or(u64::MAX) {
            return Err(JsValue::from("out of cu limit"));
         }
